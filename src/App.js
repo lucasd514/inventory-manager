767adde448540607f329f69d7984ee1a482b9d6e
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-09-10 11:28:24 -0400

    set up initial core

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import DashBoard from "./components/DashBoard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Daje!</p>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/dashboard">
+        <DashBoard />
+      </Route>
+    </Router>
   );
 }
 
